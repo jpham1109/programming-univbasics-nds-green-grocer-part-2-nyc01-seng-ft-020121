6ad6c873d2b8a9d2465ef25963102c342e6b0fd2
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-15 19:27:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -2,29 +2,7 @@ require_relative './part_1_solution.rb'
 require 'pry'
 
 def apply_coupons(cart, coupons)
-  couponed_item = {}
-  cart.each do |item|
-    coupons.each do |coupon|
-      #binding.pry
-      if ((item[:item] == coupon[:item]) && (item[:count] >= coupon[:num]))
-        if item[:count] % coupon[:num] == 0 
-          item[:item] = "#{item[:item]} W/COUPON"
-          item[:price] = coupon[:cost] / coupon[:num]
-        elsif (item[:count] % coupon[:num]) != 0 
-        #binding.pry
-          couponed_item[:item] = "#{item[:item]} W/COUPON"
-          couponed_item[:count] = item[:count] - item[:count] % coupon[:num]
-          couponed_item[:price] = coupon[:cost] / coupon[:num]
-          couponed_item[:clearance] = item[:clearance]
-        #binding.pry
-          cart << couponed_item
-          item[:count] = item[:count] % coupon[:num]
-        #binding.pry
-        end 
-      end
-    end 
-  end 
-  cart 
+  
 end
 
 def apply_clearance(cart)
