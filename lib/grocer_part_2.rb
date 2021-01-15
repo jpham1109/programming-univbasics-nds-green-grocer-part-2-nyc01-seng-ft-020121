d54b789db7d2ebcc1dbdadee3d2beaa324b51122
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-15 20:20:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,8 +4,8 @@ require 'pry'
 def apply_coupons(cart, coupons)
   cart.each do |item|
     coupons.each do |coupon|
-      if ((item[:item] == coupon[:item]) && (item[:count] >= coupon[:num]))
-        if (item[:count] % coupon[:num]) == 0
+      if item[:item] == coupon[:item] && item[:count] >= coupon[:num]
+        if item[:count] % coupon[:num] == 0
           couponed_item = {}
           couponed_item[:item] = "#{item[:item]} W/COUPON"
           couponed_item[:count] = coupon[:num]
@@ -13,10 +13,10 @@ def apply_coupons(cart, coupons)
           couponed_item[:clearance] = item[:clearance]
           item[:count] = item[:count] - coupon[:num]
           cart << couponed_item
-        elsif (item[:count] % coupon[:num]) != 0 
+        elsif item[:count] % coupon[:num] != 0 
           couponed_item = {}
           couponed_item[:item] = "#{item[:item]} W/COUPON"
-          couponed_item[:count] = item[:count] - (item[:count] % coupon[:num])
+          couponed_item[:count] = item[:count] - item[:count] % coupon[:num]
           couponed_item[:price] = coupon[:cost] / coupon[:num]
           couponed_item[:clearance] = item[:clearance]
           item[:count] = item[:count] - couponed_item[:count]
