1c2b332ff6faf868e8969c8f10953bb26d7ea887
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-15 20:21:03 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -29,9 +29,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.each do |item|
 end
 
 def checkout(cart, coupons)
